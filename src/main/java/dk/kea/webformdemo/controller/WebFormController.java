cabdc994a59a7365ede8d2f00f1f92a067272a60
jarltuxen <jarl.tuxen@gmail.com>
2022-09-14 10:22:50 +0200

    index page

---
+++ src/main/java/dk/kea/webformdemo/controller/WebFormController.java
@@ -0,0 +1,13 @@
+package dk.kea.webformdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebFormController {
+
+    @GetMapping("/")
+    public String showForm(){
+        return "webform";
+    }
+}
